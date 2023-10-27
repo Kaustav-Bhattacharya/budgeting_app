e4fe585c6ebb05371532aeec32c9b0f83ff870ee
Kaustav-Bhattacharya <bhattacharyar64@gmail.com>
2023-10-27 22:25:03 +0530

    code refactored

--- src/components/addExpenseForm.tsx
+++ src/components/addExpenseForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import  { useEffect, useRef } from "react";
 import { Form, useFetcher } from "react-router-dom";
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
 
